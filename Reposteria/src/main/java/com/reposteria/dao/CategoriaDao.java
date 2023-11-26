c86d13a5961a5e27b0afe55442f6309903017ccf
Ricardo Artavia Solano <rartaviasolano@gmail.com>
2023-11-26 16:42:47 -0600

    latest commit

--- Reposteria/src/main/java/com/reposteria/dao/CategoriaDao.java
+++
@@ -1,9 +0,0 @@
-
-package com.reposteria.dao;
-
-import com.reposteria.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaDao extends JpaRepository <Categoria, Long>{
-    
-}
